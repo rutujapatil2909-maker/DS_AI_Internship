10162d3ab799c284215aa1abeaa451446d00a60e
rutujapatil2909-maker <rutujapatil2909@gmail.com>
2026-02-04 13:48:33 +0530

    Update day2_fundamentals.py

--- src/day2_fundamentals.py
+++ src/day2_fundamentals.py
@@ -1,1 +1,10 @@
 
+item_name = "Laptop"   # String
+quantity = 2           # Integer
+price = 499.99         # Float
+in_stock = True        # Boolean
+
+print("Item Name:", item_name)
+print("Quantity:", quantity)
+print("Price:", price)
+print("In Stock:", in_stock)
