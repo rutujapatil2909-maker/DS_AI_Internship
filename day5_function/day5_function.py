0dac0fe26910fb2ecf5f921d559cc498f255ab93
rutujapatil2909-maker <rutujapatil2909@gmail.com>
2026-02-06 10:23:28 +0530

    Create day5_function.py

---
+++ day5_function/day5_function.py
@@ -0,0 +1,5 @@
+git add day5_function
+git commit -m "Add day5 function task"
+  git remote add origin https://github.com/rutujapatil2909-maker/DS_AI_Internship.git 
+git push -u origin main
+https://github.com/rutujapatil2909-maker/DS_AI_Internship/tree/main/day5_function
